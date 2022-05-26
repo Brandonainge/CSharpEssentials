711bf496910b59ca61f25fbd35cbffe077402567
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-05-26 10:47:23 -0600

    Followed the tutorials and try it yourself, and copied what I did into this practice code.

---
+++ Basics/001 Conventions and Syntax/Lab1Practice.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Brandon Ainge";
+      Console.WriteLine(name);  
+      int number = 30000;
+      Console.WriteLine(number);
+     }
+  }
+}
+
