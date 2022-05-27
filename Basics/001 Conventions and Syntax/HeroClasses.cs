18bc9e19a67f2f11215f260ba407f2c1ea86c953
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-05-26 20:38:01 -0600

    I think I added way more than I should have, but I added 2 new villains and made them very over powered in terms of higher integers.

--- Basics/001 Conventions and Syntax/HeroClasses.cs
+++ Basics/001 Conventions and Syntax/HeroClasses.cs
@@ -6,11 +6,15 @@ public class Program
 {
 	public Hero heroOne;
 	public Hero heroTwo;
+	public Villain villainOne;
+	public Villain villainTwo;
 	
 	public void Main()
 	{
 		heroOne = new Hero();
 		heroTwo = new Hero();
+		villainOne = new Villain();
+		villainTwo = new Villain();
 		
 		heroOne.health = 2;
 		heroOne.speed = 5;
@@ -21,7 +25,16 @@ public class Program
 		heroTwo.speed = 5;
 		heroTwo.height = 3;
 		heroTwo.powerLevel = 1;
-		
+
+		villainOne.health = 100;
+		villainOne.speed = 500;
+		villainOne.height = 30;
+		villainOne.powerLevel = 100;
+
+		villainTwo.health = 200;
+		villainTwo.speed = 1;
+		villainTwo.height = 100;
+		villainTwo.powerLevel = 200;
 		
 		Console.WriteLine(heroOne.health);
 		Console.WriteLine(heroOne.speed);
@@ -32,6 +45,18 @@ public class Program
 		Console.WriteLine(heroTwo.speed);
 		Console.WriteLine(heroTwo.height);
 		Console.WriteLine(heroTwo.powerLevel);
+
+		Console.WriteLine(villainOne.health);
+		Console.WriteLine(villainOne.speed);
+		Console.WriteLine(villainOne.height);
+		Console.WriteLine(villainOne.powerLevel);
+
+
+		Console.WriteLine(villainTwo.health);
+		Console.WriteLine(villainTwo.speed);
+		Console.WriteLine(villainTwo.height);
+		Console.WriteLine(villainTwo.powerLevel);
+
 	}
 }
 
@@ -40,4 +65,11 @@ public class Hero {
 	public int powerLevel;
 	public int speed;
 	public int height;	
+}
+
+public class Villain {
+	public int health;
+	public int powerLevel;
+	public int speed;
+	public int height;
 }
