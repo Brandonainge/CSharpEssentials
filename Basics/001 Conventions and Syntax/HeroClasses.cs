05dc107503bc1255062877b09f1a99613d987154
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-05-26 20:26:46 -0600

    I added speed and height to both hero one and hero two. Still a little confused on operators

--- Basics/001 Conventions and Syntax/HeroClasses.cs
+++ Basics/001 Conventions and Syntax/HeroClasses.cs
@@ -13,15 +13,24 @@ public class Program
 		heroTwo = new Hero();
 		
 		heroOne.health = 2;
+		heroOne.speed = 5;
+		heroOne.height = 7;
 		heroOne.powerLevel = 5;
 		
 		heroTwo.health = 3;
+		heroTwo.speed = 5;
+		heroTwo.height = 3;
 		heroTwo.powerLevel = 1;
 		
+		
 		Console.WriteLine(heroOne.health);
+		Console.WriteLine(heroOne.speed);
+		Console.WriteLine(heroOne.height);
 		Console.WriteLine(heroOne.powerLevel);
 		
 		Console.WriteLine(heroTwo.health);
+		Console.WriteLine(heroTwo.speed);
+		Console.WriteLine(heroTwo.height);
 		Console.WriteLine(heroTwo.powerLevel);
 	}
 }
@@ -29,4 +38,6 @@ public class Program
 public class Hero {
 	public int health;
 	public int powerLevel;
+	public int speed;
+	public int height;	
 }
