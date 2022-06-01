459b67ae3ca05a3336798fd1bc96c873cbb4c1e9
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-05-31 22:14:40 -0600

    Fixed the errors and checked on .net fiddle

--- Basics/002FunctionsAndConditionals/ConditionalsLab.cs
+++ Basics/002FunctionsAndConditionals/ConditionalsLab.cs
@@ -41,3 +41,21 @@ public class Operations {
 		}
 	}
 }
+
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (20 > 18) 
+      if (19 < 20)
+      {
+        Console.WriteLine("20 is greater than 18");
+        Console.WriteLine("19 is less than 20");
+      }    
+    }
+  }
+}
+
