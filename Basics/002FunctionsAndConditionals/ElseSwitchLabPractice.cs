061c34ebc3b0e01360bfdd6579a5aa9a6f8c15d4
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-06-01 18:55:41 -0600

    updating Added an else statement

---
+++ Basics/002FunctionsAndConditionals/ElseSwitchLabPractice.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int height = 70;
+      if (height < 90) 
+      {
+        Console.WriteLine("You are short.");
+      } 
+      else 
+      {
+        Console.WriteLine("I am short.");
+      }
+    }
+  }
+}
--- Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
+++ Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
@@ -1,4 +1,45 @@
 // The Expectation is that you write your own code based
 // on what you have learned in the class, it can be what you want
-// If you are havind trouble thinging of something, write conditionals
+// If you are havind trouble thinking of something, write conditionals
 // with the topic of PowerUps, what happens if the powerUp is good or bad.
+
+
+
+using System;
+					
+public class Program
+{
+	public PowerUp powerUp;
+	
+	public void Main()
+	{
+		powerUp = new PowerUp();
+		powerUp.currentState = GameStates.States.Ending;
+		powerUp.CheckState();
+	}
+}
+
+public class GameStates {
+	
+	public enum States {
+		Starting,
+		Playing,
+		Ending
+	}
+	
+	public States currentState = States.Starting;
+	
+	public void CheckState () {
+		switch (currentState) {
+			case States.Starting:
+				Console.WriteLine("Starting");
+				break;
+			case States.Playing:
+				Console.WriteLine("Playing");
+				break;
+			case States.Ending:
+				Console.WriteLine("Ending");
+				break;
+		}
+	}
+}
