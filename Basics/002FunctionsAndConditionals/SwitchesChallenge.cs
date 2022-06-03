766235df8e1cb357996d1e6b682be388b50aa3dd
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-06-02 19:21:41 -0600

    I created an if else condition where the users experience is rewarded for having more experience by giving them better power ups

--- Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
+++ Basics/002FunctionsAndConditionals/SwitchesChallenge.cs
@@ -4,42 +4,22 @@
 // with the topic of PowerUps, what happens if the powerUp is good or bad.
 
 
-
 using System;
 					
 public class Program
 {
-	public PowerUp powerUp;
-	
 	public void Main()
 	{
-		powerUp = new PowerUp();
-		powerUp.currentState = GameStates.States.Ending;
-		powerUp.CheckState();
-	}
-}
-
-public class GameStates {
-	
-	public enum States {
-		Starting,
-		Playing,
-		Ending
-	}
-	
-	public States currentState = States.Starting;
-	
-	public void CheckState () {
-		switch (currentState) {
-			case States.Starting:
-				Console.WriteLine("Starting");
-				break;
-			case States.Playing:
-				Console.WriteLine("Playing");
-				break;
-			case States.Ending:
-				Console.WriteLine("Ending");
-				break;
-		}
+		Console.WriteLine ("Power Up");
+		int experience = 30;
+        int exeprience2 = 40;
+		if (experience > exeprience2)
+        {
+        Console.WriteLine ("Great Power up");
+        }
+        else
+        {
+        Console.WriteLine ("Bad Power Up");
+        }
 	}
 }
