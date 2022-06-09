baa32265344235a87bff17888d23212d2e41542c
Brandonainge <105466798+Brandonainge@users.noreply.github.com>
2022-06-08 19:09:38 -0600

    Created a for loop with an array about armor

---
+++ Basics/003_1C_Lab/ForLoopsWithArraysLab.cs
@@ -0,0 +1,13 @@
+using System;
+
+ public class Program
+  {
+    public static void Main()
+    {
+      string[] armor = {"Helmet", "Chestplate", "Pants", "Boots"};
+      for (int i = 0; i < armor.Length; i++) 
+      {
+        Console.WriteLine(armor[i]);
+      }
+    }
+  }
